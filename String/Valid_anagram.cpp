14fe1fdf814c9585a09c0983e2c931ff9f44ed9e
Tushar Bhardwaj <tusharbhardwaj.313@gmail.com>
2022-03-10 02:15:11 +0530

    Create Valid_anagram.cpp
    Question no: 242
    Given two strings s and t, return true if t is an anagram of s, and false otherwise.
    An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.

---
+++ String/Valid_anagram.cpp
@@ -0,0 +1,14 @@
+//link : https://leetcode.com/problems/valid-anagram/
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size())
+            return false;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        if(s == t)
+            return true;
+        else
+            return false;
+    }
+};
