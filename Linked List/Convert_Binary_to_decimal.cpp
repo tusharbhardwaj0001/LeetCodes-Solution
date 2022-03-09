a9df64e657b509950c1f5cc67f511bd245bd4c42
Tushar Bhardwaj <tusharbhardwaj.313@gmail.com>
2022-03-09 12:52:25 +0530

    Create Convert_Binary_to_decimal.cpp
    Question no:  1290
    
    Given head which is a reference node to a singly-linked list. The value of each node in the linked list is either 0 or 1. The linked list holds the binary representation of a number.
    
    Return the decimal value of the number in the linked list.

---
+++ Linked List/Convert_Binary_to_decimal.cpp
@@ -0,0 +1,25 @@
+//Link : https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int getDecimalValue(ListNode* head) {
+        int no = 0;
+        ListNode* temp = head;
+        while(temp != NULL)
+        {
+            no = no*2 + temp->val;
+            temp = temp->next;
+        }
+        return no;
+    }
+};
